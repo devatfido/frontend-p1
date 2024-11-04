73b447531deff3bab67fbe3460be851ffe54408e
pavanatfido <pavankumarkpkp@gmail.com>
2024-11-04 23:18:42 +0530

    add abi

---
+++ src/abi/SBT.ts
@@ -0,0 +1,25 @@
+export const SBTMintABI = [
+    {
+        "inputs": [
+            {
+                "internalType": "address",
+                "name": "to",
+                "type": "address"
+            },
+            {
+                "internalType": "string",
+                "name": "metaURI",
+                "type": "string"
+            },
+            {
+                "internalType": "bytes32",
+                "name": "claimHashMetadata",
+                "type": "bytes32"
+            }
+        ],
+        "name": "mint",
+        "outputs": [],
+        "stateMutability": "nonpayable",
+        "type": "function"
+    }
+]
