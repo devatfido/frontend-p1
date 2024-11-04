ea7c1a5d743e68c4a256576387e4be515e22a416
pavanatfido <pavankumarkpkp@gmail.com>
2024-11-04 22:48:02 +0530

    setup rainbow kit

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { ConnectButton } from '@rainbow-me/rainbowkit';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
+    <main className="App">
+      <h1>Get Identity</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+          <ConnectButton />
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        Connect your wallet and get your identity by minting a SBT Token.
       </p>
-    </>
+    </main>
   )
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,56 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import {
+    createBrowserRouter,
+    RouterProvider,
+} from "react-router-dom";
 import './index.css'
 import App from './App.tsx'
+import React from 'react';
+import '@rainbow-me/rainbowkit/styles.css';
+import {
+    getDefaultConfig,
+    RainbowKitProvider,
+} from '@rainbow-me/rainbowkit';
+import { WagmiProvider } from 'wagmi';
+import {
+    polygonAmoy
+} from 'wagmi/chains';
+import {
+    QueryClientProvider,
+    QueryClient,
+} from "@tanstack/react-query";
+
+const config = getDefaultConfig({
+    appName: 'Fido Poc',
+    projectId: 'YOUR_PROJECT_ID',
+    chains: [polygonAmoy],
+    ssr: false,
+});
+const queryClient = new QueryClient();
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <App />,
+    },
+    {
+        path: "/issuer",
+        element: <div>issuer</div>,
+    },
+    {
+        path: "/get-token",
+        element: <div>get-token</div>,
+    },
+]);
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+    <React.StrictMode>
+        <WagmiProvider config={config}>
+            <QueryClientProvider client={queryClient}>
+                <RainbowKitProvider>
+                    <RouterProvider router={router} />
+                </RainbowKitProvider>
+            </QueryClientProvider>
+        </WagmiProvider>
+    </React.StrictMode>
 )
